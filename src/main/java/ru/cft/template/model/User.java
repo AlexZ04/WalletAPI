b70d38c73ed46e35dfffa01b1fc20da2a44134f0
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-30 15:08:01 +0700

    Добавлены ограничения целостности пользователя в бд

--- src/main/java/ru/cft/template/model/User.java
+++ src/main/java/ru/cft/template/model/User.java
@@ -1,16 +1,23 @@
 package ru.cft.template.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Data
+@Entity
+@Table(name = "users")
 public class User {
-    int id;
+    @Id
+    @GeneratedValue
+    Long id;
     String lastName;
     String firstName;
     String middleName;
+    @Column(unique = true)
     String phone;
+    @Column(unique = true)
     String email;
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     String birthday;
