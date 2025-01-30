61c1dc338f554360817629cca0074bebbfbbf3c5
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-01-31 06:38:20 +0700

    Добавлено получение кошелька по пользователю

--- src/main/java/ru/cft/template/repository/WalletRepository.java
+++ src/main/java/ru/cft/template/repository/WalletRepository.java
@@ -1,7 +1,9 @@
 package ru.cft.template.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ru.cft.template.model.User;
 import ru.cft.template.model.Wallet;
 
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Wallet foundByUser(User user);
 }
--- src/main/java/ru/cft/template/service/impl/WalletServiceImpl.java
+++ src/main/java/ru/cft/template/service/impl/WalletServiceImpl.java
@@ -3,7 +3,16 @@ package ru.cft.template.service.impl;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.cft.template.dto.wallet.WalletDto;
+import ru.cft.template.exception.SessionNotFoundException;
+import ru.cft.template.exception.UnauthorizedException;
+import ru.cft.template.exception.UserNotFoundException;
+import ru.cft.template.model.Session;
+import ru.cft.template.model.User;
+import ru.cft.template.model.Wallet;
+import ru.cft.template.repository.SessionRepository;
+import ru.cft.template.repository.UserRepository;
 import ru.cft.template.repository.WalletRepository;
+import ru.cft.template.service.SessionService;
 import ru.cft.template.service.WalletService;
 
 import java.util.UUID;
@@ -12,11 +21,24 @@ import java.util.UUID;
 @AllArgsConstructor
 public class WalletServiceImpl implements WalletService {
     private final WalletRepository walletRepository;
+    private final UserRepository userRepository;
+    private final SessionRepository sessionRepository;
+    private final SessionService sessionService;
 
     @Override
     public WalletDto getWalletInfo(Long userId, UUID sessionId) {
-        //todo
-        return null;
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found"));
+
+        Session session = sessionRepository.findById(sessionId)
+                .orElseThrow(() -> new SessionNotFoundException("Session not found"));
+
+        if (sessionService.checkSession(session)) {
+            throw new UnauthorizedException("You session expired");
+        }
+
+        Wallet wallet = walletRepository.foundByUser(user);
+
+        return new WalletDto(wallet);
     }
 
     @Override
